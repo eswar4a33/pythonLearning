6bbdd81ed7a243ad79b96cfa12281211364a0c2f
eswar <eswar4a33@gmail.com>
2025-05-22 22:07:14 +0530

    adding optimised pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -3,19 +3,25 @@
 *   *   2    3    4
 *   5   6    7    8    9
 10  11  12   13   14   15  16
-
-
-
 """
-height = int(input("enter the no. lines: "))
-x = 1
 
-for i in range(height):
-    print("   "*(height-i),end="")
-    for j in range(i+1):
-        print(x," ",end="")
-        x += 1
-    for k in range(i):
-        print(x," " ,end ="")
-        x += 1
-    print()
+def pyr(height):
+    num = 1
+    maz = len(str(height*height))
+
+        
+    for i in range(1,height+1):
+        print(" "*(maz+1)*(height-i),end="")
+        for j in range(i):
+            print(" "*(maz-len(str(num))),end="")
+            print(num,end=" ")
+            num += 1
+        for k in range(i-1):
+            print(" "*(maz-len(str(num))),end ="")
+            print(num,end=" ")
+            num += 1
+        print()
+for hei in range(22):
+    pyr(hei)
+
+
