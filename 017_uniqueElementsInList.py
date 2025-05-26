1a4c2b45d279c4f6c96914dab4ee82e871fc58a2
eswar <eswar4a33@gmail.com>
2025-05-27 01:07:40 +0530

    fixed logic 017_uniqueElementsInList.py

--- 017_uniqueElementsInList.py
+++ 017_uniqueElementsInList.py
@@ -6,9 +6,13 @@ list e.g [1,2,4,1,3,4,5,6,5] should give [2,3,6]"""
 ls = [1,2,4,1,3,4,5,6,5]
 lss = []
 
-for _ in range(len(ls)):
-    x = ls.pop(0)
-    if x not in ls:
-        lss.append(x)
+for i in ls:
+    count = 0
+    for j in ls:
+        if i == j:
+            count += 1
+            
+    if count == 1:
+        lss.append(i)    
 print(lss)
 
