55e4ce057f9e2e4d6183cc1d539d96a305d656e5
eswar <eswar4a33@gmail.com>
2025-06-14 17:51:33 +0530

    adding 010_fizzbuzzFunction.py

--- 010_fizzbuzzFunction.py
+++ 010_fizzbuzzFunction.py
@@ -1,29 +1,20 @@
+def fizzbuzz(num):
+    res = ""
+    if num % 3 == 0 and num % 5 == 0:
+        res = "FizzBuzz"
+    elif num % 3 == 0:
+        res = "Fizz"
+    elif num % 5 == 0:
+        res = "Buzz"
+    else:
+        res = num
+    return res
+for i in range(1,101):
+    print(fizzbuzz(i))
+    
+        
 
-"""
-import math
-a = 10
-b = 10
-print(math.sqrt(pow(a,2)+pow(a,2)))
 
 
-"""
 
-def add(a,b,c):
-    print(a,b,c)
-    r = a+b+c
-    a = 5
-    b = 6
-    c = 7
-    print(a,b,c)
-    return a,b,c
-
-a = 10
-b = 20
-c = 30
-print(a,b,c)
-e,f,g = add(a,b,c)
-a,b,c = add(a,b,c)
-
-print(e,f,g)
-print(a,b,c)
-
+    
