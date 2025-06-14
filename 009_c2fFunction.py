095085d807bd474ccdd37b4df70a63ce2fc7f604
eswar <eswar4a33@gmail.com>
2025-06-14 17:12:04 +0530

    adding 009_c2fFunction.py

--- 009_c2fFunction.py
+++ 009_c2fFunction.py
@@ -1,29 +1,5 @@
-
-"""
-import math
-a = 10
-b = 10
-print(math.sqrt(pow(a,2)+pow(a,2)))
-
-
-"""
-
-def add(a,b,c):
-    print(a,b,c)
-    r = a+b+c
-    a = 5
-    b = 6
-    c = 7
-    print(a,b,c)
-    return a,b,c
-
-a = 10
-b = 20
-c = 30
-print(a,b,c)
-e,f,g = add(a,b,c)
-a,b,c = add(a,b,c)
-
-print(e,f,g)
-print(a,b,c)
-
+celsius = float(input('enter the temperature in celsius: '))
+def c2f(temp):
+    fahrenheit = (9/5*temp)+32
+    return fahrenheit
+print(c2f(celsius))
