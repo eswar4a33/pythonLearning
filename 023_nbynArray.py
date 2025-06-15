9d4f0ae7b7bce63dad58232d786b34ec059b6126
eswar <eswar4a33@gmail.com>
2025-06-15 21:17:02 +0530

    adding 023_nbynArray.py

--- 023_nbynArray.py
+++ 023_nbynArray.py
@@ -1,8 +1,8 @@
 n = int(input('enter a number: '))
+
 ls = []
 ls1 = []
 for i in range(1,n*n+1):
-
     print(f"{i:2}",end = " ")
     ls1.append(i)
     if i % n == 0:
@@ -10,5 +10,21 @@ for i in range(1,n*n+1):
         ls.append(ls1)
         ls1 = []
 
-for i in range(len(ls)):
-    print(ls[i])
+center = start = end = n//2
+l = []
+while center >= 0:
+    for i in range(start, end+1):
+        for j in range(start, end+1):
+            inx = str(i)+str(j)
+            if inx not in l:
+                l.append(inx)
+                print(i,j,end = " ")
+        
+    print()
+    start -= 1
+    end += 1
+    center -= 1
+
+
+
+
